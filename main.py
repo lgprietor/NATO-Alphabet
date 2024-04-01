e8fbc015bf01518dccf5ca49ec35e4c8c6f2ca0e
Luis Gabriel <luisgabrielprieto@gmail.com>
2024-03-31 21:44:09 -0500

    Commit

--- main.py
+++ main.py
@@ -2,6 +2,39 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
+dictionary = {row.letter:row.code for (index,row) in data.iterrows()}
+# print(dictionary)
+
+user_name = input("Please insert a user name: \n").upper()
+
+split_names = user_name.split()
+
+for i in range(len(split_names)):
+    new_list_i = [n.capitalize() for n in split_names[i]]
+    new_list_2i = [dictionary[letter] for letter in new_list_i]
+
+    # dictionary_2 = {letter:dictionary[letter] for letter in new_list_i}
+    #
+    # print(dictionary_2)
+
+    # print(new_list_i)
+    print(new_list_2i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # letters = data["letter"]
 #
 # print(letters)
@@ -16,13 +49,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 # print(type(dictionary))
 
-dictionary = {index:row for (index,row) in data.iterrows()}
+# dictionary = {key:value for (key,value) in data.items()}
 
-print(dictionary)
 
-user_name = input("Please insert a user name: \n").lower()
+# dictionary = {row.letter:row.code for (index,row) in data.iterrows()}
 
-split_names = user_name.split()
+# print(dictionary)
+
+# user_name = input("Please insert a user name: \n").lower()
+#
+# split_names = user_name.split()
 
 # capitalize(): mayúscula la primera letra
 # upper(): mayúsculas toda la cadena
@@ -30,12 +66,12 @@ split_names = user_name.split()
 # new_list = [n.upper() for n in split_names]
 # print(new_list)
 
-for i in range(len(split_names)):
-    new_list_i = [n.capitalize() for n in split_names[i]]
-
+# for i in range(len(split_names)):
+#     new_list_i = [n.capitalize() for n in split_names[i]]
 #
-#
-#     print(new_list_i)
-    print(new_list_2i)
+# #
+# #
+# #     print(new_list_i)
+#     print(new_list_2i)
 
 
